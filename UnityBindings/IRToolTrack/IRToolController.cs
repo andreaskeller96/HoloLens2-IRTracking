b160678cf03ab76f97e670009ed60814533e4fde
Andreas Keller <andi.keller@tum.de>
2023-09-06 16:57:12 +0200

    Add Unity C# Bindings

---
+++ UnityBindings/IRToolTrack/IRToolController.cs
@@ -0,0 +1,168 @@
+﻿using Microsoft.MixedReality.Toolkit;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+namespace IRToolTrack
+{
+    public class IRToolController : MonoBehaviour
+    {
+        
+        public string identifier;
+        public GameObject[] spheres;
+        public bool disableUntilDetection = false;
+        public bool disableWhenTrackingLost = false;
+        public float secondsLostUntilDisable = 3;
+        public float sphere_radius = 6.5f;
+        public int max_occluded_spheres = 0;
+        public float lowpass_factor_rotation = 0.3f;
+        public float lowpass_factor_position = 0.6f;
+
+        private bool childrenActive = true;
+
+        private IRToolTracking irToolTracking;
+        private Int64 lastUpdate = 0;
+        private float lastSpotted = 0;
+        private Vector3 targetPosition = Vector3.zero;
+        private Quaternion targetRotation = Quaternion.identity;
+        private List<Vector3> positions = new List<Vector3>();
+        private List<Quaternion> rotations = new List<Quaternion>();
+        private bool[] childAtIndexActive;
+        public int sphere_count
+        {
+            get { return spheres.Length; }
+        }
+
+        public float[] sphere_positions
+        {
+            get {
+                float[] coordinates = new float[sphere_count*3];
+                int cur_coord = 0;
+                for (int i = 0; i< sphere_count; i++) {
+                    coordinates[cur_coord] = spheres[i].transform.localPosition.x;
+                    coordinates[cur_coord + 1] = spheres[i].transform.localPosition.y;
+                    coordinates[cur_coord + 2] = spheres[i].transform.localPosition.z;
+                    cur_coord += 3;
+                }
+                return coordinates;
+            }
+        }
+
+
+        void Start()
+        {
+            childAtIndexActive = new bool[transform.childCount];
+            irToolTracking = FindObjectOfType<IRToolTracking>();
+#if !UNITY_EDITOR
+            if (disableUntilDetection)
+            {
+                for (int i = 0; i<transform.childCount; i++)
+                {
+                    var curChild = transform.GetChild(i).gameObject;
+                    if (curChild.activeSelf)
+                    {
+                        childAtIndexActive[i] = true;
+                        curChild.SetActive(false);
+                    }
+                }
+                childrenActive = false;
+            }
+#endif
+        }
+
+        bool LoadROMFile(string romFilePath)
+        {
+            var romFile = Resources.Load(romFilePath);
+            return false;
+        }
+
+        public enum Status
+        {
+            Inactive,
+            Active
+        }
+        private Status _subStatus = Status.Inactive;
+
+        public void StartTracking()
+        {
+            if (_subStatus == Status.Active)
+            {
+                Debug.Log("Tool tracking already started.");
+                return;
+            }
+            //_listener.Start();
+            Debug.Log("Started tracking "+identifier);
+            _subStatus = Status.Active;
+        }
+
+        public void StopTracking()
+        {
+            if (_subStatus == Status.Inactive)
+            {
+                Debug.Log("Tracking of "+identifier+" already stopped.");
+                return;
+            }
+            //_listener.Stop();
+            Debug.Log("Stopped tracking " + identifier);
+            _subStatus = Status.Inactive;
+        }
+
+        void Update()
+        {
+            if (_subStatus == Status.Inactive)
+                return;
+            Int64 trackingTimestamp = irToolTracking.GetTimestamp();
+            float[] tool_transform = irToolTracking.GetToolTransform(identifier);
+            if (tool_transform != null && tool_transform[0]!= float.NaN && tool_transform[7]!=0 && lastUpdate<trackingTimestamp)
+            {
+                if (!childrenActive)
+                {
+                    for (int i = 0; i < transform.childCount; i++)
+                    {
+                        var curChild = transform.GetChild(i).gameObject;
+                        if (childAtIndexActive[i])
+                        {
+                            curChild.SetActive(true);
+                        }
+                    }
+                    childrenActive = true;
+                }
+
+                Quaternion q = new Quaternion(tool_transform[3], tool_transform[4], tool_transform[5], tool_transform[6]);
+                targetRotation = q;
+                targetPosition = new Vector3(tool_transform[0], tool_transform[1], tool_transform[2]);
+                lastSpotted = Time.time;
+            }
+            else if (childrenActive && disableWhenTrackingLost && Time.time-lastSpotted>secondsLostUntilDisable)
+            {
+                for (int i = 0; i < transform.childCount; i++)
+                {
+                    transform.GetChild(i).gameObject.SetActive(false);
+                }
+                childrenActive = false;
+            }
+
+
+            /*
+            //Delay Positioning by one frame to maybe make it smoother
+            if (lastUpdate == trackingTimestamp)
+            {
+                transform.position = targetPosition;
+                //transform.rotation = Quaternion.Lerp(targetRotation, transform.rotation, 0);
+            }
+            else
+            {
+                transform.position = Vector3.Lerp(targetPosition, transform.position, 0.5f);
+                //transform.rotation = Quaternion.Lerp(targetRotation, transform.rotation, 0.5f);
+            }
+            */
+            transform.position = targetPosition;
+            transform.rotation = targetRotation;
+            lastUpdate = trackingTimestamp;
+        }
+    }
+}
---
+++ UnityBindings/IRToolTrack/IRToolTracking.cs
@@ -0,0 +1,120 @@
+﻿
+using UnityEngine;
+using System;
+using IRToolTrack;
+using System.Linq;
+
+
+#if ENABLE_WINMD_SUPPORT
+using System.Threading.Tasks;
+using HL2IRToolTracking;
+#endif
+
+public class IRToolTracking : MonoBehaviour
+{
+#if ENABLE_WINMD_SUPPORT
+    HL2IRTracking toolTracking;
+#endif
+    private bool startToolTracking = false;
+
+    private IRToolController[] tools = null;
+
+    public float[] GetToolTransform(string identifier)
+    {
+        var toolTransform = Enumerable.Repeat<float>(0, 8).ToArray();
+#if ENABLE_WINMD_SUPPORT
+        toolTransform = toolTracking.GetToolTransform(identifier);        
+#endif
+        return toolTransform;
+
+    }
+
+    public Int64 GetTimestamp()
+    {
+#if ENABLE_WINMD_SUPPORT
+        return toolTracking.GetTrackingTimestamp();
+#else
+        return 0;
+#endif
+    }
+
+    public void Start()
+    {
+        //Find Tool Controllers and add them to the tracking
+        tools = FindObjectsOfType<IRToolController>();
+        StartToolTracking();
+    }
+
+    public void StartToolTracking()
+    {
+            Debug.Log("Start Tracking");
+#if ENABLE_WINMD_SUPPORT
+        if (!startToolTracking){
+            if (toolTracking == null)
+            {
+                toolTracking = new HL2IRTracking();
+            }
+            SetReferenceWorldCoordinateSystem();
+            toolTracking.RemoveAllToolDefinitions();
+            foreach (IRToolController tool in tools)
+            {
+                int min_visible_spheres = tool.sphere_count;
+                if (tool.max_occluded_spheres > 0 && (tool.sphere_count - tool.max_occluded_spheres) >= 3)
+                {
+                    min_visible_spheres = tool.sphere_count - tool.max_occluded_spheres;
+                }
+                toolTracking.AddToolDefinition(tool.sphere_count, tool.sphere_positions, tool.sphere_radius, tool.identifier, min_visible_spheres, tool.lowpass_factor_rotation, tool.lowpass_factor_position);
+                tool.StartTracking();
+            }
+            toolTracking.StartToolTracking();
+            startToolTracking = true;
+        }
+#endif
+    }
+
+    public void StopToolTracking()
+    {
+        if (!startToolTracking)
+        {
+            return;
+        }
+
+#if ENABLE_WINMD_SUPPORT
+        toolTracking.StopToolTracking();
+        startToolTracking = false;
+        foreach (IRToolController tool in tools)
+        {
+            tool.StopTracking();
+        }
+#endif
+        Debug.Log("Stopped Tracking");
+    }
+
+    private void SetReferenceWorldCoordinateSystem()
+    {
+        print("Setting World Coordinate");
+#if ENABLE_WINMD_SUPPORT
+        // Get Unity Origin Coordinate
+#if UNITY_2021_2_OR_NEWER
+        //var unityWorldOrigin = Microsoft.Windows.Perception.Spatial.SpatialCoordinateSystem
+        Windows.Perception.Spatial.SpatialCoordinateSystem unityWorldOrigin = Microsoft.MixedReality.OpenXR.PerceptionInterop.GetSceneCoordinateSystem(UnityEngine.Pose.identity) as Windows.Perception.Spatial.SpatialCoordinateSystem;
+#elif UNITY_2020_1_OR_NEWER
+        IntPtr WorldOriginPtr = UnityEngine.XR.WindowsMR.WindowsMREnvironment.OriginSpatialCoordinateSystem;
+        var unityWorldOrigin = Marshal.GetObjectForIUnknown(WorldOriginPtr) as Windows.Perception.Spatial.SpatialCoordinateSystem;
+#else
+        IntPtr WorldOriginPtr = UnityEngine.XR.WSA.WorldManager.GetNativeISpatialCoordinateSystemPtr();
+        var unityWorldOrigin = Marshal.GetObjectForIUnknown(WorldOriginPtr) as Windows.Perception.Spatial.SpatialCoordinateSystem;
+#endif
+        // Set Unity Origin Coordinate
+        toolTracking.SetReferenceCoordinateSystem(unityWorldOrigin);
+#endif
+
+
+    }
+
+    public void ExitApplication()
+    {
+        StopToolTracking();
+        Application.Quit();
+    }
+}
